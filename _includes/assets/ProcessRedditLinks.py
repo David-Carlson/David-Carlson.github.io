2061ce52e002443b6c14a8f928d6554e7a20199f
Sylvernale <david.carlson2012@gmail.com>
2017-10-18 15:52:36 -0400

    Simplified links

--- _includes/assets/ProcessRedditLinks.py
+++ _includes/assets/ProcessRedditLinks.py
@@ -3,20 +3,16 @@ import re
 def ProcessLine(pattern, line):
     match = re.search(pattern, line)
     if match is None:
-        return line + "------------------------------------------------------------------------------\n"
+        return line + "*" * 79 + "\n"
     else:
         words = match.group(1).replace("-", " ")
-        # This return formats in the reddit style
-        #return f'* [{words}]({line[:-1]})\n'
-        # This one formats in html anchor tags
-        #return f'<a href="{line[:-1]}">{words}</a>\n'
         # This one simply puts a line between text and link so pastebin can use it
         return f'{words}\n\t\t{line}\n'
 
 # www.udemy.com/applewatchcourse/?couponCode=EnrollFREE
 if __name__ == '__main__':
+    # Extract the text after '.com/' and the next slash
     pattern = re.compile(r'^www\.udemy\.com[/]([^/]+)[/]')
-    # pattern = re.compile(r'^www\.udemy\.com')
     with open('links.txt', 'r') as read:
         with open('fixedlinks.txt', 'w' ) as write:
             for line in read:
